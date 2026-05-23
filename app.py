5abbf7e46957b499f3c26e9d04f83349ebf67186
Mahmoud8833 <mahmoud.a.ali.88@gmail.com>
2026-05-23 17:37:43 +0300

    Add new files

---
+++ app.py
@@ -0,0 +1,6 @@
+word = "Hello"
+name = "World"
+sentence = word + " " + name
+print(sentence)
+word = "Goodbye"
+print(sentence)
---
+++ test.py
@@ -0,0 +1,6 @@
+age = input("How old are you? ")
+if int(age) >=18:
+    print("You can enter :)")
+else:
+    print("Get back home kid.")
+print("Work Done, Lets enjoy the party!!!")
