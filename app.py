4eb756ba14ab533eede683b31068959a5cc05d91
Mahmoud8833 <mahmoud.a.ali.88@gmail.com>
2026-05-23 21:39:15 +0300

    Using While and For Loops

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-word = "Hello"
-name = "World"
-sentence = word + " " + name
-print(sentence)
-word = "Goodbye"
-print(sentence)
+#Using Concatination
+# word = "Hello"
+# name = "World"
+# sentence = word + " " + name
+# print(sentence)
+# word = "Goodbye"
+# print(sentence)
+
+#Using While Loop
+# num = 0
+# sum = 0
+# while num < 6:
+#     sum = sum + num
+#     num += 1
+
+# print(sum)
+
+sum = 0
+for i in range(5):
+    sum = sum + i
+
+print(sum)
